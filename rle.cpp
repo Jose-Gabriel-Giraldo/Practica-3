2626d00d67abb1cfa0a65141620324613dfa71e0
Jose-Gabriel-Giraldo <josegabriel.giraldo@udea.edu.co>
2026-09-22 18:40:43 -0500

    Se modifica para dejarse solo las funcionalidades directas de rle

--- rle.cpp
+++ rle.cpp
@@ -1,41 +1,8 @@
 #include <iostream>
 #include "RLE.h"
+#include "utilidades.h"
 
 using namespace std;
-
-int longitudTexto(const char* texto) {
-    int contador = 0;
-    while (texto[contador] != '\0') {
-        contador++;
-    }
-    return contador;
-}
-
-bool textosIguales(const char* a, const char* b) {
-    int i = 0;
-    while (a[i] != '\0' && b[i] != '\0') {
-        if (a[i] != b[i]) {
-            return false;
-        }
-        i++;
-    }
-    return a[i] == b[i];
-}
-
-bool esDigito(char c) {
-    return c >= '0' && c <= '9';
-}
-
-// Cuenta cuantos digitos tiene un numero (ej: 12 -> 2 digitos)
-int contarDigitos(int n) {
-    int digitos = 1;
-    while (n >= 10) {
-        n /= 10;
-        digitos++;
-    }
-    return digitos;
-}
-
 
 char* comprimirRLE(const char* texto) {
     int longitud = longitudTexto(texto);
@@ -84,7 +51,6 @@ char* comprimirRLE(const char* texto) {
     salida[pos] = '\0';
     return salida;
 }
-
 
 char* descomprimirRLE(const char* comprimido) {
     int longitud = longitudTexto(comprimido);
